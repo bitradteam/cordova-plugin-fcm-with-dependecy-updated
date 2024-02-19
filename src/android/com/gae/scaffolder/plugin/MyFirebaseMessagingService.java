356e7869f5230745b3d78fc061110ea429d40f03
promonteiro <promonteiro@mc.pt>
2024-02-19 19:03:21 +0000

    MyFirebaseMessagingService small fix

--- src/android/com/gae/scaffolder/plugin/MyFirebaseMessagingService.java
+++ src/android/com/gae/scaffolder/plugin/MyFirebaseMessagingService.java
@@ -14,7 +14,6 @@ import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 import com.salesforce.marketingcloud.MarketingCloudSdk;
 import com.salesforce.marketingcloud.messages.push.PushMessageManager;
-
 import java.util.HashMap;
 import java.util.Map;
 
@@ -40,68 +39,25 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
   public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
     Log.d(TAG, "==> MyFirebaseMessagingService onMessageReceived");
     if (PushMessageManager.isMarketingCloudPush(remoteMessage)) {
-      MarketingCloudSdk.requestSdk(marketingCloudSdk -> marketingCloudSdk.getPushMessageManager().handleMessage(remoteMessage));
+      MarketingCloudSdk
+          .requestSdk(marketingCloudSdk -> marketingCloudSdk.getPushMessageManager().handleMessage(remoteMessage));
     } else {
       try {
         Map<String, String> messageData = remoteMessage.getData();
-        
-        FCMPlugin.sendPushPayload(buildNotificationData(remoteMessage));
-      } catch (NotInitializedException | InvalidInputException | NotRegisteredException e) {
+        sendNotification(buildNotificationData(remoteMessage));
+      } catch (Exception e) {
         e.printStackTrace();
       }
     }
   }
-}
 
-  private Map<String, Object> buildNotificationData(RemoteMessage remoteMessage) {
-    Map<String, Object> data = new HashMap<>();
-
+  private Map<String, String> buildNotificationData(RemoteMessage remoteMessage) {
+    Map<String, String> data = new HashMap<>();
     for (String key : remoteMessage.getData().keySet()) {
-      Object value = remoteMessage.getData().get(key);
+      String value = remoteMessage.getData().get(key);
       data.put(key, value);
     }
     Log.d(TAG, "\tNotification Data: " + data);
     return data;
   }
-  /*
-   * private void buildNotification(String channel, String title, String message)
-   * {
-   * 
-   * Intent intent = new Intent(this, MainActivity.class);
-   * intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK |
-   * Intent.FLAG_ACTIVITY_CLEAR_TASK);
-   * PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
-   * 
-   * createNotificationChannel(this, channel);
-   * 
-   * NotificationCompat.Builder builder = new NotificationCompat.Builder(this,
-   * channel)
-   * .setPriority(NotificationCompat.PRIORITY_HIGH)
-   * .setSmallIcon(R.drawable.apdu_service_banner)
-   * .setContentTitle(title)
-   * .setContentText(message)
-   * .setContentIntent(pendingIntent)
-   * .setAutoCancel(true);
-   * 
-   * NotificationManagerCompat notificationManager =
-   * NotificationManagerCompat.from(this);
-   * notificationManager.notify(69, builder.build());
-   * }
-   * 
-   * private void createNotificationChannel(Context context, String channelId) {
-   * // Create the NotificationChannel, but only on API 26+ because
-   * // the NotificationChannel class is new and not in the support library
-   * if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-   * NotificationChannel channel = new NotificationChannel(channelId, channelId,
-   * NotificationManager.IMPORTANCE_HIGH);
-   * channel.setDescription(channelId);
-   * // Register the channel with the system; it's not possible to change the
-   * importance
-   * // or other notification behaviors after this
-   * NotificationManager notificationManager =
-   * context.getSystemService(NotificationManager.class);
-   * notificationManager.createNotificationChannel(channel);
-   * }
-   * }
-   */
 }
