e2689ff0723f77ba4a62678ed377512ce70c13cb
promonteiro <promonteiro@mc.pt>
2024-02-19 20:21:00 +0000

    Changed onMessageReceived code

--- src/android/com/gae/scaffolder/plugin/MyFirebaseMessagingService.java
+++ src/android/com/gae/scaffolder/plugin/MyFirebaseMessagingService.java
@@ -12,8 +12,8 @@ import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
-import com.salesforce.marketingcloud.MarketingCloudSdk;
 import com.salesforce.marketingcloud.messages.push.PushMessageManager;
+import com.salesforce.marketingcloud.sfmcsdk.SFMCSdk;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -24,9 +24,9 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
   @Override
   public void onNewToken(@NonNull String token) {
     super.onNewToken(token);
-    Log.d(TAG, "New Firebase token: " + token);
-    FCMPlugin.sendTokenRefresh(token);
-    MarketingCloudSdk.requestSdk(marketingCloudSdk -> marketingCloudSdk.getPushMessageManager().setPushToken(token));
+    SFMCSdk.requestSdk(sdk -> {
+      sdk.mp(it -> it.pushMessageManager.setPushToken(token));
+    });
   }
 
   /**
@@ -38,16 +38,27 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
   @Override
   public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
     Log.d(TAG, "==> MyFirebaseMessagingService onMessageReceived");
+
     if (PushMessageManager.isMarketingCloudPush(remoteMessage)) {
-      MarketingCloudSdk
-          .requestSdk(marketingCloudSdk -> marketingCloudSdk.getPushMessageManager().handleMessage(remoteMessage));
+      SFMCSdk.requestSdk(sdk -> {
+        sdk.mp(it -> it.pushMessageManager.handleMessage(remoteMessage));
+      });
     } else {
-      try {
-        Map<String, String> messageData = remoteMessage.getData();
-        sendNotification(buildNotificationData(remoteMessage));
-      } catch (Exception e) {
-        e.printStackTrace();
+      if (remoteMessage.getNotification() != null) {
+        Log.d(TAG, "\tNotification Title: " + remoteMessage.getNotification().getTitle());
+        Log.d(TAG, "\tNotification Message: " + remoteMessage.getNotification().getBody());
       }
+
+      Map<String, Object> data = new HashMap<String, Object>();
+      data.put("wasTapped", false);
+      for (String key : remoteMessage.getData().keySet()) {
+        Object value = remoteMessage.getData().get(key);
+        Log.d(TAG, "\tKey: " + key + " Value: " + value);
+        data.put(key, value);
+      }
+
+      Log.d(TAG, "\tNotification Data: " + data.toString());
+      FCMPlugin.sendPushPayload(data);
     }
   }
 
